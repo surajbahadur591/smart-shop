675fbf44cfcdc892b55fdb073d4f9e0ee8cb9b73
Suraj Bahadur <surajbahadur58@gmail.com>
2020-10-18 23:06:36 +0530

    added products in front page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,16 @@ import React from 'react';
 import {Container} from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
-
+import HomeScreen from '../src/screens/HomeScreen'
 
 const App = ()=> {
   return (
     <>
     <Header/>
-    <main className="py-3">
+    <main className="py-3 ">
       <Container>
-      <h1>Welcome to Smart Shop</h1>
+      {/* <h1>Welcome to Smart Shop</h1> */}
+      <HomeScreen/>
       </Container>
     
     </main>
