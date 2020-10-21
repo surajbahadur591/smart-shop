2026c45f80831878436763dbb32ac3f01ced4342
Suraj Bahadur <surajbahadur58@gmail.com>
2020-10-21 13:00:28 +0530

    adding redux store

---
+++ backend/routes/productRoute.js
@@ -0,0 +1,37 @@
+// import  Router  from "express"
+import express from 'express'
+import asyncHandler from 'express-async-handler'
+
+import Product from "../models/productModel.js"
+
+//@desc Fetch all products
+//@route GET /api/products
+//@acess Public
+
+const router = express.Router()
+
+router.get('/', asyncHandler(async(req, res) => {
+    const products = await Product.find({})
+
+    
+    res.json(products)
+}))
+
+//@desc Fetch single products
+//@route GET /api/products/:id
+//@acess Public
+router.get('/:id', asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id)
+
+    if(product){
+        res.json(product)
+    } 
+    else {
+        res.status(404)
+        throw new Error ('product not found')
+    }
+    
+}))
+
+
+export default router
--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,12 @@
 import express from 'express'
 import dotenv from 'dotenv'
 // const products = require("./data/products.js")
-import products from './data/products.js'
+// import products from './data/products.js'
 import connectDB from './config/db.js'
 import colors from 'colors'
+import {notFound, errorHandler} from './middleware/errorMiddleware.js'
+
+import productRoute from './routes/productRoute.js'
 
 dotenv.config()
 
@@ -12,20 +15,20 @@ connectDB()
 
 const app = express()
 
+
+
 app.get('/', (req, res) => {
 
     res.send('API is running..')
 })
 
-app.get('/api/products', (req, res) => {
 
-    res.json(products)
-})
+app.use('/api/products', productRoute)
 
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find(p => p._id === req.params.id)
-    res.json(product)
-})
+app.use(notFound)
+
+app.use(errorHandler )
+
 
 const PORT = process.env.PORT || 5000
 
